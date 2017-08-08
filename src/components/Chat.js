92e98e174275e42c16015afa361ce0feffe94a81
R S <oseng@e2r1p5.42.fr>
2017-08-08 15:21:41 +0200

    [ch31]  chat finit, il reste a penser le systeme de discussion seulemt avec les gens likes

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -6,36 +6,44 @@ import {browserHistory} from "react-router";
 export default class Chat extends React.Component {
 	constructor() {
 		super();
-		this.state = {login: "Admin"};
+		this.state = {
+			login : "Admin",
+			down  : "false"
+	};
+	}
+	down(e) {
+		console.log("close mesenger");
+		// ('#downChat').fadOut();
 	}
 
 	render() {
 		return (
-			<div id="myChat">
-				<Navbar>
-				<Col xs={12} md={12}>
-					<div>
-						<div id="users">
-						</div>
+			<Col xs={12} md={3} id="myChat">
+				{/* <i className="fa fa-angle-down" aria-hidden="true">x</i> */}
+					<div id="downChat" onClick={(e) => {this.down(e)}}>x</div>
+					<div id="messages">
+						<div>
+							<div id="users">
+							</div>
 
-						<div id="messages">
-							<p>message : </p>
-							<span>date : </span>
 						</div>
+					</div>
 						<Col id="loginform">
 							<form action="" id="formulaireChat">
 								<input type="text" id="login" name="login" placeholder="login"  className="form-control1"></input>
+								{/* <input type="text" id="txt" name="txt" placeholder="message"  className="form-control1"></input> */}
 								<input type="mail" id="mail" name="mail" placeholder="m@il"  className="form-control1"></input>
 								<input type="submit" value="Send" className="btn btn-primary"></input>
 							</form>
 						</Col>
-						<div>
-
-						</div>
-					</div>
+						<Col id="form">
+							<form action="" id="formulaireChat">
+								<input type="text" id="login" name="login" placeholder="login"  className="form-control1" hidden></input>
+								<input type="text" id="message" name="message" placeholder="message"  className="form-control1"></input>
+								<input type="submit" value="Send" className="btn btn-primary"></input>
+							</form>
+						</Col>
 				</Col>
-			</Navbar>
-		</div>
-	);
-}
+		);
+	}
 }
--- static/js/client.js
+++ static/js/client.js
@@ -1,20 +1,57 @@
 (function($){
 	var socket = io.connect('http://localhost:3000');
+	var lastmsg = false;
 
 
 	$('#loginform').submit(function(event){
 		event.preventDefault();
 		//on emet un evenemtn cote cient pour le servuer
 		socket.emit('login', {
-			username  : $('#login').val(),
-			mail	  : $('#mail').val()
+			pseudo	  : $('#login').val(),
+			mail	  : $('#mail').val(),
+			// txt	  	  : $('#txt').val()
 			})
 	});
-		// on recoit un event cote client provenant du serveur
-		socket.on('newuser', function(user) {
-			// alert('new user');//test alert OK
-			$('#users').append('<h3>' + user.id + '</h3>');
-			$('#users').append('<img style="height:2em;"src="' + 'https://cdn.intra.42.fr/users/medium_default.png' + '"/>');
+
+		socket.on('logged', function() {
+			$('#loginform').fadeOut();
 		})
+
+//================ envoie de messages =============
+	$('#form').submit(function(event) {
+		event.preventDefault();// couper l;evenement
+
+	socket.emit('newmsg', {
+			pseudo	  : $('#login').val(),
+			message	  : $('#message').val(),
+		})
+		$('#message').val('');//on supprime le message precedent
+		$('#message').focus();//on refocus sur ce chammp
+	});
+
+//on affiche les messages
+	socket.on('newmsg', function(msg) {
+		// $('#users').append('<img style="height:2em;" src="' + 'https://cdn.intra.42.fr/users/medium_default.png' + '"/>');// recuperer la photo
+		$('#messages').append('<h3> Pseudo </h3>')//recuperer le pseudo de l'user
+		$('#messages').append('<div  style="word-wrap: break-word;">' + msg.message + '</div>')
+		$('#messages').animate({scrollTop : $('#messages').prop('scrollHeight' + '3em') }, 50);//scroll auo en bas des messages
+	})
+
+
+//================ gestion des connectee =============
+
+	// on recoit un event cote client provenant du serveur
+	socket.on('newusr', function(user) {
+		// alert('new user');//test alert OK
+		// $('#users').append('<h3>' + user.id + '</h3>');
+		$('#users').append('<h4>' + user.login + '</h4>');
+		// $('#users').append('<span>' + user.txt + '</span>');
+		$('#users').append('<img style="height:2em;" id="' + user.id + '"  src="' + 'https://cdn.intra.42.fr/users/medium_default.png' + '"/>');
+	})
+
+	//si un user est deconnecte, on l'enleve
+	socket.on('discusr',function(user) {
+		$('#' + user.id).remove();
+	})
 
 })(jQuery);
